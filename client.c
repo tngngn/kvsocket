b448a49e492afe409dde9073b5cac6a0561caaa2
tngngn <tungnguyen173371@gmail.com>
2022-08-22 01:33:08 +0900

    properly complete

--- client.c
+++ client.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
+#include <arpa/inet.h>
+
+#define MAX_INPUT_SIZE 256
+#define PORT 4444
 
 void error(char *msg)
 {
@@ -12,45 +17,61 @@ void error(char *msg)
     exit(0);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+
     int sockfd, portno, n;
-
     struct sockaddr_in serv_addr;
-    struct hostent *server;
 
-    char buffer[256];
+    char buffer[MAX_INPUT_SIZE];
     if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       fprintf(stderr,"usage %s <server-ip-addr> <server-port>\n", argv[0]);
        exit(0);
     }
+
     portno = atoi(argv[2]);
-    sockfd = socket(AF_LOCAL, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
+
+    /* Create client socket */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        fprintf(stderr, "ERROR opening socket\n");
+        exit(1);
     }
+    
     bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_LOCAL;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
+    serv_addr.sin_family = AF_INET;
+    if(!inet_aton(argv[1], &serv_addr.sin_addr)) {
+        fprintf(stderr, "ERROR invalid server IP address\n");
+        exit(1);
+    }
     serv_addr.sin_port = htons(portno);
+
     if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
+    printf("Connected to server\n");
+
+    while(1) {
+        printf("enter command: ");
+        bzero(buffer,MAX_INPUT_SIZE);
+	    fgets(buffer,MAX_INPUT_SIZE-1,stdin);
+
+        n = write(sockfd,buffer,strlen(buffer));
+        if (n < 0) 
+            error("ERROR writing to socket");
+
+        if(buffer[0]=='q') {
+            close(sockfd);
+            printf("disconnected from server\n");
+            exit(1);
+        }
+
+        bzero(buffer,MAX_INPUT_SIZE);
+        n = read(sockfd,buffer,MAX_INPUT_SIZE-1);
+        if (n < 0) 
+            error("ERROR reading from socket");
+        printf("server: %s\n",buffer);
+
+    } 
+
     return 0;
 }
+
